bed09b7224fed123679cd61af0f3d5a6ca9bae88
Juan Charris <Juan.Charris2110@gmail.com>
2025-03-18 14:58:59 -0500

    a

--- src/configuration/DB_config.js
+++ src/configuration/DB_config.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost', 
-    user: 'root', 
-    password: '', 
-    database: 'db_node', 
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'db_node',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
